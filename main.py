08e9ee245539f7ee774eab7d55f665f28753849b
Noach <trainer.hedge@gmail.com>
2024-08-03 22:20:48 +0300

    Victory

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 import qrcode
 import socket
 
@@ -13,10 +14,45 @@ def print_qr_address(data):
 
     qr.print_ascii()
 
-def create_connection(port):
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', port))
-    s.listen()
-    conn, address = s.accept()
+def open_connection(port):
+    host = ''
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.bind((host, port))
+        print(f'Listening at port {port}...')
+        s.listen(1)
+        conn, addr = s.accept()
+        with conn:
+            print('Connected by', addr)
+            while True:
+                data = conn.recv(1024)
+
+                print(f'Received {data.decode()}')
+
+                if not data or data.decode() == 'Q':
+                    break
+                if data.decode() == 'S':
+                    conn.sendall(b'Ack')
+                    data = conn.recv(1024)
+
+                    print(f'Received {data.decode()}')
+
+                    conn.sendall(b'Ack')
+                    f_name, num_bytes = data.decode().split(':')
+                    if '.' in f_name and num_bytes.isdigit():
+                        with open(f'./test-receive/{f_name}', 'wb') as f:
+                            received_bytes = 0
+                            while received_bytes < int(num_bytes):
+                                data = conn.recv(1024)    
+                                f.write(data)
+                                received_bytes += len(data)
+                        conn.sendall(b'Fin')
+                    else:
+                        conn.sendall(b'Inv')
+                else:
+                    conn.sendall(b'Inv')
+
+open_connection(17701)
+
+        
     
 
---
+++ test_client.py
@@ -0,0 +1,26 @@
+import math
+import socket
+import os
+
+filename = './test-send/TestSubjectNo1.jpg'
+
+HOST = '127.0.0.1'
+PORT = 17701
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect((HOST, PORT))
+    while True:
+        s.sendall(b'S')
+        ack = s.recv(1024)
+
+        print('Received Ack 1')
+
+        filesize = os.path.getsize(filename)
+        s.sendall(f'{filename.split('/')[-1]}:{filesize}'.encode())
+        ack = s.recv(1024)
+
+        print('Received Ack 2')
+
+        with open(filename, 'rb') as f:
+            while read_bytes := f.read(1024):
+                s.sendall(read_bytes)
+        input()
