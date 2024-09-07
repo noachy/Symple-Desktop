1549165da8a5efbb8c0b8052f0f0b7689f6dbcff
Noach <trainer.hedge@gmail.com>
2024-09-07 15:34:24 +0300

    Added main ui page

---
+++ README.md
@@ -0,0 +1,9 @@
+# A symple-desktop Flet app
+
+An example of a minimal Flet app.
+
+To run the app:
+
+```
+flet run [app_directory]
+```
--- main.py
+++ main.py
@@ -1,81 +1,19 @@
-import math
+import flet as ft
 import qrcode
-import socket
+import base64
 
-from tqdm import tqdm
-
-BUFFER_SIZE = 1024
+from io import BytesIO
 
 
-def print_qr_address(data):
-    qr = qrcode.QRCode(
-        version=1,
-        error_correction=qrcode.constants.ERROR_CORRECT_L,
-        box_size=10,
-        border=4,
-    )
-    qr.add_data(data)
-    qr.make(fit=True)
+def main(page: ft.Page):
+    qr_img = qrcode.make('This is a Code!', border=2)
+    buffered = BytesIO()
+    qr_img.save(buffered, format='JPEG')
+    qr_str_b64 = base64.b64encode(buffered.getvalue()).decode('utf-8')
 
-    qr.print_ascii()
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.add(ft.Container(ft.Image(src_base64=qr_str_b64)))
 
 
-def app():
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:  # TODO : wrap socket with TLS
-        s.bind(('', 0))
-        port = s.getsockname()[1]
-        local_address = socket.gethostbyname_ex(socket.gethostname())[2][1]
-        print('Scan the following QR code on the mobile app in order to establish a connection: ')
-        # implement deep-links
-        print_qr_address(f'{local_address}:{port}')
-        print(f'Listening at {local_address}:{port}...')
-        while True:
-            s.listen(1)
-            conn, addr = s.accept()
-            print("\033[2J\033[H", end="", flush=True)
-            print(f'Connected to {addr[0]}, waiting for files...')
-            with conn:
-                while True:
-                    data = conn.recv(1)
-
-                    if not data or data.decode() == 'Q':
-                        print('Connection terminated.')
-                        break
-                    if data.decode() == 'S':
-                        conn.sendall(b'AckCom')
-                        data = conn.recv(BUFFER_SIZE)
-
-                        f_name, num_bytes = data.decode().split(':')
-
-                        if '.' in f_name and num_bytes.isdigit():
-                            num_bytes = int(num_bytes)
-
-                            with open(f'./test-receive/{f_name}', 'wb') as f:
-                                print(f'Receiving {f_name}')
-                                conn.sendall(b'AckFle')
-                                received_bytes = 0
-                                bytes_from_last_got = 0
-
-                                with tqdm(total=num_bytes) as prog_bar:
-                                    while received_bytes < num_bytes:
-                                        data = conn.recv(BUFFER_SIZE)
-                                        f.write(data)
-                                        received_bytes += len(data)
-                                        bytes_from_last_got += len(data)
-
-                                        if bytes_from_last_got > 333333:
-                                            conn.sendall(
-                                                (f'GOT {received_bytes / num_bytes} ').encode())
-                                            bytes_from_last_got = 0
-
-                                        prog_bar.update(BUFFER_SIZE)
-                                print('Done!')
-                            conn.sendall(b'Fin')
-                        else:
-                            conn.sendall(b'Inv : not digit')
-                    else:
-                        conn.sendall(b'Inv : data is not S or Q')
-
-
-if __name__ == '__main__':
-    app()
+ft.app(main)
